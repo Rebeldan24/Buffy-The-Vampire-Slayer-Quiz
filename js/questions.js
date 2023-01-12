54b5c471d076dedf70f7ae90497d2441edc18b0c
Rebeldan24 <djsol86@gmail.com>
2023-01-12 11:32:13 +0000

    addedd questions

---
+++ js/questions.js
@@ -0,0 +1,65 @@
+//creating an array and passing the number, questions, option and answers
+
+let questions = [
+    {
+        numb: 1,
+        question: "What does HTML stand for ?"
+        answer: "Hyper Text markup language"
+        options: [
+            "Hyper Text Prep",
+            "Hyper Text markup language",
+            "Hyper Text Language",
+            "Hyper Tool Langugage"
+
+
+        ]
+    },
+    {
+        numb:2,
+        question: "What does CSS stand for ?",
+        answer: "Cascading Style Sheet",
+        options: [
+            "Hyper Text Prep",
+            "Hyper Text markup language",
+            "Hyper Text Language",
+            "Cascading Style Sheet",
+        ]
+
+    },
+    {
+        numb:3,
+        question: "What does PHP stand for ?",
+        answer: "Hypertext Preprocessor",
+        options: [
+            "Hypertext Preprocessor",
+            "Hyper Text markup language",
+            "Hyper Text Language",
+            "Cascading Style Sheet",
+        ]
+
+    },
+    {
+        numb:4,
+        question: "What does SQL stand for ?",
+        answer: "Structured Query Language",
+        options: [
+            "Hypertext Preprocessor",
+            "Hyper Text markup language",
+            "Hyper Text Language",
+            "Structured Query Language",
+        ]
+
+    },
+    {
+        numb:5,
+        question: "What does XML stand for ?",
+        answer: "Extensive markup Language",
+        options: [
+            "Extensive markup Language",
+            "Hyper Text markup language",
+            "Hyper Text Language",
+            "Structured Query Language",
+        ]
+
+    },
+];
