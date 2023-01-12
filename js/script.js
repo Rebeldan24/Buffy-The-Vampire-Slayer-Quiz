508c383ba006ee85c6b7bf517d6aa919e19a1693
Rebeldan24 <djsol86@gmail.com>
2023-01-12 11:05:53 +0000

    added js to buttons exit continue

---
+++ js/script.js
@@ -0,0 +1,24 @@
+//getting all required elements
+const start_btn = document.querySelector(".start_btn button");
+const info_box = document.querySelector(".info_box");
+const exit_btn = info_box.querySelector(".buttons .quit");
+const continue_btn = info_box.querySelector(".buttons .restart");
+const quiz_box = info_box.querySelector(".quiz_box");
+
+
+//If Start Quiz Button CLicked
+start_btn.onclick = ()=>{
+    info_box.classList.add("activeInfo"); //show the info box
+}
+
+//If exit button clicked
+
+exit_btn.onclick = ()=>{
+    info_box.classList.remove("activeInfo"); //hide the info box
+}
+
+//if continue button clicked
+continue_btn.onclick = ()=>{
+    info_box.classList.remove("activeInfo"); //hide the info box
+    quiz_box.classList.remove("activeQuiz"); //hide the info box
+}
