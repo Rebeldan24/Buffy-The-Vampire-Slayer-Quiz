dbbf4bebaef6fcc22d69295977f39fd725a2ae8f
Rebeldan24 <djsol86@gmail.com>
2023-01-17 11:47:38 +0000

    dff

--- js/questions.js
+++ js/questions.js
@@ -2,7 +2,7 @@ let MCQS = [{
     question: "What does HTML stand for?",
     choice1: "Hyperlinks and Text Markup Language",
     choice2: "Hyper Text Markup Language",
-    choice3: "Hyper Text Making Langauge",
+    choice3: "Hyper Text Making Language",
     choice4: "Hyper Text Mark Language",
     answer: 1
 },
@@ -16,14 +16,82 @@ question: "What does CSS stand for?",
 
 },
 {
-    question: "Whixh HTML tag is used to dfine an internal style sheetr?",
-        choice1: "Colorful Stylesheet",
-        choice2: "Creative Style Sheet",
-        choice3: "Cascading Style Sheet",
-        choice4: "Computer Style Sheet",
-        answer: 2
+question: "Which HTML tag is used to define an internal style sheet?",
+        choice1: "<script>",
+        choice2: "<style>",
+        choice3: "<html>",
+        choice4: "<svg>",
+        answer: 1
+    
+},
+
+{
+question: "What is the correct CSS syntax?",
+            choice1: "body{color:black}",
+            choice2: "{body{color:black}",
+            choice3: "body={color:black}",
+            choice4: "body:color{black}",
+            answer: 0
+},
+{
+question: "How do you insert a comment in a Css file?",
+                choice1: "/*This is comment*/",
+                choice2: "//This is comment",
+                choice3: "<!--- This is comment --->",
+                choice4: "//This is comment//",
+                answer: 1
+             },
+           
+{
+question: "How do you insert a comment in a HTML file?",
+                    choice1: "/*This is comment*/",
+                    choice2: "//This is comment",
+                    choice3: "<!--- This is comment --->",
+                    choice4: "//This is comment//",
+                    answer: 1
+                
+},
+{
+question: "Which property is ued tio change the background color?",
+                        choice1: "backgroundColor",
+                        choice2: "BgColor",
+                        choice3: "<Color-Background",
+                        choice4: "background",
+                        answer: 3
+                    
+ },
+                    {
+question: "How to write an If statement in Javascript?",
+                            choice1: "if i==5",
+                            choice2: "if(i==5)",
+                            choice3: "if(i==5)then",
+                            choice4: "if i==5 then",
+                            answer: 2
+                        
+},
+{
+question: "Inside which HTML element do we put the javascript",
+                                choice1: "<js></js>",
+                                choice2: "<javascript></javascript>>",
+                                choice3: "<script></script>",
+                                choice4: "<scripting>",
+                                answer: 2
+                            
+ },
+ {
+    question: "How does a While loop start?",
+        choice1: "while(i <= 0)",
+        choice2: "while(i <= 0 i++)",
+        choice3: "while i <= 0",
+        choice4: "while (i++ i <= 0)",
+        answer: 0
     
     },
+   
+                           
+                       
+               
+        
     
 
 
