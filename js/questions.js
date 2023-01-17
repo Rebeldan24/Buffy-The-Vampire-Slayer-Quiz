cc95e66784f8279e200ad35a5eab4477ea1507e7
Rebeldan24 <djsol86@gmail.com>
2023-01-17 11:06:54 +0000

    dfgh

---
+++ js/questions.js
@@ -0,0 +1,33 @@
+let MCQS = [{
+    question: "What does HTML stand for?",
+    choice1: "Hyperlinks and Text Markup Language",
+    choice2: "Hyper Text Markup Language",
+    choice3: "Hyper Text Making Langauge",
+    choice4: "Hyper Text Mark Language",
+    answer: 1
+},
+{
+question: "What does CSS stand for?",
+    choice1: "Colorful Stylesheet",
+    choice2: "Creative Style Sheet",
+    choice3: "Cascading Style Sheet",
+    choice4: "Computer Style Sheet",
+    answer: 2
+
+},
+{
+    question: "Whixh HTML tag is used to dfine an internal style sheetr?",
+        choice1: "Colorful Stylesheet",
+        choice2: "Creative Style Sheet",
+        choice3: "Cascading Style Sheet",
+        choice4: "Computer Style Sheet",
+        answer: 2
+    
+    },
+    
+
+
+
+
+
+]
